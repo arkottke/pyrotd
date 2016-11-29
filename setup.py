845f38b483b72f7b6cf5b19351659a5fcbc308f7
Albert Kottke <albert.kottke@gmail.com>
2016-11-29 14:13:50 -0800

    New setup.py.

--- setup.py
+++ setup.py
@@ -1,15 +1,60 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-setup(name='pyrotd',
-      version='0.1',
-      description=
-      'Computes the normal and rotated pseudo-spectral acceleration'
-      'of earthquake ground motions',
-      author='Albert Kottke',
-      author_email='albert.kottke@gmail.com',
-      url='https://github.com/arkottke/pyrotd',
-      py_modules=['pyrotd',],
-      requires=['numpy'],
-     )
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'numpy',
+    'setuptools',
+]
+
+test_requirements = [
+    'coveralls',
+    'flake8',
+    'pytest',
+    'pytest-cov',
+    'pytest-flake8',
+    'pytest-runner'
+]
+
+setup(
+    name='pyrotd',
+    version='0.1.0',
+    description='Ground motion models implemented in Python.',
+    long_description=readme + '\n\n' + history,
+    author='Albert Kottke',
+    author_email='albert.kottke@gmail.com',
+    url='http://github.com/arkottke/pyrotd',
+    py_modules=['pyrotd'],
+    include_package_data=True,
+    install_requires=requirements,
+    license='MIT',
+    zip_safe=False,
+    keywords='pyrotd',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Console',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Intended Audience :: Science/Research',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
