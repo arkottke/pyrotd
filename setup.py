c008892ddb2be8238a86fd88af8c2b417a619d96
Albert Kottke <albert.kottke@gmail.com>
2017-04-06 10:50:22 -0700

    Simplified options.

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -12,42 +9,29 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [
-    'numpy',
-    'setuptools',
-]
-
-test_requirements = [
-    'coveralls', 'flake8', 'matplotlib', 'pytest', 'pytest-cov',
-    'pytest-flake8', 'pytest-runner'
-]
-
 setup(
     name='pyrotd',
-    version='0.2.0',
-    description='Ground motion models implemented in Python.',
+    version='0.3.0',
+    description='Rotated response spectrum calculation implemented in Python.',
     long_description=readme + '\n\n' + history,
     author='Albert Kottke',
     author_email='albert.kottke@gmail.com',
     url='http://github.com/arkottke/pyrotd',
-    py_modules=['pyrotd'],
-    include_package_data=True,
-    install_requires=requirements,
-    extras_require={
-        'plots': ['matplotlib'],
-    },
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'setuptools',
+    ],
     license='MIT',
-    zip_safe=False,
-    keywords='pyrotd',
     classifiers=[
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
-        'Environment :: Console',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Scientific/Engineering',
         'Intended Audience :: Science/Research',
     ],
     test_suite='tests',
-    tests_require=test_requirements)
+)
