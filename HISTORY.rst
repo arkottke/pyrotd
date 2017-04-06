617c8d808120b26229403a03e0ecd1129d2019e6
Albert Kottke <albert.kottke@gmail.com>
2017-04-05 21:02:59 -0700

    Updated version and history.

--- HISTORY.rst
+++ HISTORY.rst
@@ -1,6 +1,11 @@
 History
 =======
 
+0.3.0 (2017-04-05)
+------------------
+- Added example
+- Added support for typing
+
 0.2.0 (2017-02-20)
 ------------------
 - Changed from camelCase to underscores
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='pyrotd',
-    version='0.2.0',
+    version='0.3.0',
     description='Ground motion models implemented in Python.',
     long_description=readme + '\n\n' + history,
     author='Albert Kottke',
