ef27660d15e343abb5d38d581f490e9d9afb7772
Albert Kottke <albert.kottke@gmail.com>
2017-04-06 21:06:26 -0700

    Increment version.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('HISTORY.rst') as history_file:
 
 setup(
     name='pyrotd',
-    version='0.3.3',
+    version='0.4.0',
     description='Rotated response spectrum calculation implemented in Python.',
     long_description=readme + '\n\n' + history,
     author='Albert Kottke',
