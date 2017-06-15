d1569f658f4a5639b2bba35046b25b0b7df96c2e
Albert Kottke <albert.kottke@gmail.com>
2017-06-15 11:10:40 -0700

    Move metadata from setup.cfg to setup.py.

--- setup.py
+++ setup.py
@@ -1,6 +1,38 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
+import os
 
 from setuptools import setup
 
-setup()
+here = os.path.abspath(os.path.dirname(__file__))
+# Get the long description from the README file
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as fp:
+    long_description = fp.read()
+
+setup(
+    name='pyrotd',
+    license='MIT',
+    version='0.5.0-dev',
+    description='Rotated response spectrum calculation implemented in Python.',
+    long_description=long_description,
+    url='http://github.com/arkottke/pyrotd',
+    author='Albert Kottke',
+    author_email='albert.kottke@gmail.com',
+    install_requires=[
+        'numpy',
+        'setuptools',
+    ],
+    test_suite='tests',
+    keywords=['response spectrum', 'earthquake ground motion'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering',
+        'Intended Audience :: Science/Research',
+    ],
+    zip_safe=True,
+    include_package_data=True,
+)
