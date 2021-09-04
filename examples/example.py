cae0ab0ac9be3878d844502c1ccc2d422599d190
Albert Kottke <albert.kottke@gmail.com>
2021-09-04 15:18:25 -0700

    Migrate code style to Black

--- examples/example.py
+++ examples/example.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
-
 import os
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 import pyrotd
+
 pyrotd.processes = 1
 
 # Load the AT2 timeseries
 fname = os.path.join(
-    os.path.dirname(__file__), 'test_data/RSN8883_14383980_13849360.AT2')
+    os.path.dirname(__file__), "test_data/RSN8883_14383980_13849360.AT2"
+)
 with open(fname) as fp:
     for _ in range(3):
         next(fp)
@@ -29,10 +30,11 @@ fig, ax = plt.subplots()
 ax.plot(resp_spec.osc_freq, resp_spec.spec_accel)
 
 ax.set(
-    xlabel='Frequency (Hz)',
-    xscale='log',
-    ylabel='5%-Damped Spectral Accel. (g)',
-    yscale='log', )
+    xlabel="Frequency (Hz)",
+    xscale="log",
+    ylabel="5%-Damped Spectral Accel. (g)",
+    yscale="log",
+)
 ax.grid()
 fig.tight_layout()
 plt.show(fig)
--- tests/test_spectra.py
+++ tests/test_spectra.py
@@ -2,13 +2,13 @@ import gzip
 import json
 import os
 
+import matplotlib
 import numpy as np
 import pytest
 
 import pyrotd
 
-import matplotlib
-matplotlib.use('Agg')
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt  # NOQA
 
 # Need to disable multiprocessing for pytest
@@ -20,12 +20,12 @@ def get_relpath(fname):
     return os.path.join(os.path.dirname(__file__), fname)
 
 
-with gzip.open(get_relpath('data/peer_nga_west2.json.gz')) as fp:
+with gzip.open(get_relpath("data/peer_nga_west2.json.gz")) as fp:
     records = json.load(fp)
 
 
 def load_at2(fname):
-    fpath = get_relpath('data/' + fname)
+    fpath = get_relpath("data/" + fname)
     with open(fpath) as fp:
         for _ in range(3):
             next(fp)
@@ -39,33 +39,44 @@ def load_at2(fname):
 def iter_single_cases():
     """Iterate single response spectra with a single motion."""
     for record in records:
-        osc_freqs = 1 / np.array(record['period'])
-        time_step, accels_a = load_at2(record['fnames'][0])
-        accels_b = load_at2(record['fnames'][1])[1]
+        osc_freqs = 1 / np.array(record["period"])
+        time_step, accels_a = load_at2(record["fnames"][0])
+        accels_b = load_at2(record["fnames"][1])[1]
 
-        for spectrum in record['spectra']:
-            osc_damping = spectrum['damping']
-            for key, accels in zip(('h1', 'h2'), (accels_a, accels_b)):
+        for spectrum in record["spectra"]:
+            osc_damping = spectrum["damping"]
+            for key, accels in zip(("h1", "h2"), (accels_a, accels_b)):
                 if key not in spectrum:
                     continue
 
-                yield ('%s_%s_%d' % (record['rsn'], key,
-                                     100 * osc_damping), osc_damping,
-                       osc_freqs, spectrum[key], time_step, accels)
+                yield (
+                    "%s_%s_%d" % (record["rsn"], key, 100 * osc_damping),
+                    osc_damping,
+                    osc_freqs,
+                    spectrum[key],
+                    time_step,
+                    accels,
+                )
 
 
 def iter_rotated_cases():
     """Iterate rotated spectra with pairs of motions."""
     for record in records:
-        osc_freqs = 1 / np.array(record['period'])
-        time_step, accels_a = load_at2(record['fnames'][0])
-        accels_b = load_at2(record['fnames'][1])[1]
+        osc_freqs = 1 / np.array(record["period"])
+        time_step, accels_a = load_at2(record["fnames"][0])
+        accels_b = load_at2(record["fnames"][1])[1]
 
-        for spectrum in record['spectra']:
-            osc_damping = spectrum['damping']
-            yield ('%s_%s_%d' % (record['rsn'], 'rotd50',
-                                 100 * osc_damping), osc_damping, osc_freqs,
-                   spectrum['rotd50'], time_step, accels_a, accels_b)
+        for spectrum in record["spectra"]:
+            osc_damping = spectrum["damping"]
+            yield (
+                "%s_%s_%d" % (record["rsn"], "rotd50", 100 * osc_damping),
+                osc_damping,
+                osc_freqs,
+                spectrum["rotd50"],
+                time_step,
+                accels_a,
+                accels_b,
+            )
 
 
 def plot_comparison(name, osc_freqs, target, computed):
@@ -73,31 +84,30 @@ def plot_comparison(name, osc_freqs, target, computed):
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
 
-    ax1.plot(osc_freqs, computed, 'b-', label='Computed')
-    ax1.plot(osc_freqs, target, 'r--', label='Target')
+    ax1.plot(osc_freqs, computed, "b-", label="Computed")
+    ax1.plot(osc_freqs, target, "r--", label="Target")
 
     ax2 = ax1.twinx()
-    rel_diff = 100. * (computed - target) / target
+    rel_diff = 100.0 * (computed - target) / target
     ax2.plot(osc_freqs, rel_diff, linewidth=0.5)
 
-    ax1.set(
-        xscale='log', xlabel='Frequency (Hz)', yscale='log', ylabel='PSA (g)')
-    ax2.set_ylabel('Relative Difference (%)')
+    ax1.set(xscale="log", xlabel="Frequency (Hz)", yscale="log", ylabel="PSA (g)")
+    ax2.set_ylabel("Relative Difference (%)")
     ax1.grid()
     ax1.legend()
     fig.tight_layout()
-    fig.savefig('test-' + name)
+    fig.savefig("test-" + name)
     plt.close(fig)
 
 
 @pytest.mark.parametrize(
-    'name,osc_damping,osc_freqs,target,time_step,accels',
+    "name,osc_damping,osc_freqs,target,time_step,accels",
     iter_single_cases(),
 )
-def test_calc_response_spectrum(name, osc_damping, osc_freqs, target,
-                                time_step, accels):
-    resp_spec = pyrotd.calc_spec_accels(time_step, accels, osc_freqs,
-                                        osc_damping)
+def test_calc_response_spectrum(
+    name, osc_damping, osc_freqs, target, time_step, accels
+):
+    resp_spec = pyrotd.calc_spec_accels(time_step, accels, osc_freqs, osc_damping)
     computed = resp_spec.spec_accel
     if plt:
         plot_comparison(name, osc_freqs, target, computed)
@@ -105,19 +115,16 @@ def test_calc_response_spectrum(name, osc_damping, osc_freqs, target,
 
 
 @pytest.mark.parametrize(
-    'name,osc_damping,osc_freqs,target,time_step,accels_a,accels_b',
+    "name,osc_damping,osc_freqs,target,time_step,accels_a,accels_b",
     iter_rotated_cases(),
 )
-def test_calc_rotated_response_spectrum(name, osc_damping, osc_freqs, target,
-                                        time_step, accels_a, accels_b):
+def test_calc_rotated_response_spectrum(
+    name, osc_damping, osc_freqs, target, time_step, accels_a, accels_b
+):
     # Compute the rotated spectra
     rotated = pyrotd.calc_rotated_spec_accels(
-        time_step,
-        accels_a,
-        accels_b,
-        osc_freqs,
-        osc_damping,
-        percentiles=[50])
+        time_step, accels_a, accels_b, osc_freqs, osc_damping, percentiles=[50]
+    )
     computed = rotated.spec_accel
     if plt:
         plot_comparison(name, osc_freqs, target, computed)
